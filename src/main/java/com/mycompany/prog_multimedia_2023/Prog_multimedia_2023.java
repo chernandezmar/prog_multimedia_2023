f3a5f2c62174ea71e792429a882fa71b080a5749
carlangas <carlos.hernandezmar@anahuac.mx>
2023-02-16 20:45:20 -0600

    primer commit

---
+++ src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog_multimedia_2023;
+
+/**
+ *
+ * @author khemtz
+ */
+public class Prog_multimedia_2023 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola curso de programacion multimedia");
+    }
+}
