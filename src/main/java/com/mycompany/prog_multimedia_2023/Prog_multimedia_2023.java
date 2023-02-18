bb8bb0831c20e3c5c63d20a8ff0a3600206602a6
Juan Perez <juan.perez@anahuac.mx>
2023-02-17 23:42:04 -0600

    cambio desde la mac

--- src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
+++ src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
@@ -11,6 +11,7 @@ package com.mycompany.prog_multimedia_2023;
 public class Prog_multimedia_2023 {
 
     public static void main(String[] args) {
-        System.out.println("Hola curso de programacion multimedia, este es un cambio");
+        String name="carlos";
+        System.out.println("Hola curso de programacion multimedia, este es un cambio " + name);
     }
 }
