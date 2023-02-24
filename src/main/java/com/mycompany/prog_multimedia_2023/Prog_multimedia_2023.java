ed66a1434b9eba6c7e286212af0f4a8c07bc0844
carlangas <carlos.hernandezmar@anahuac.mx>
2023-02-23 20:39:29 -0600

    Declaracion y uso de floats

--- src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
+++ src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
@@ -13,9 +13,14 @@ public class Prog_multimedia_2023 {
     public static void main(String[] args) {
         int numero1=20;
         int numero2=30;
-        int res=(numero1 + numero2);
+        double decimal=10.4;
+        double res=(numero1 + numero2 + decimal);
+        
+        char letra='s';
+        String frase="hola mundo";
+        
         /*la declaracion de la variable es:
         tipo nombreVariable=valor;*/
-        System.out.println("resultado=" + res);
+        System.out.println(frase + " resultado=" + res);
     }
 }
