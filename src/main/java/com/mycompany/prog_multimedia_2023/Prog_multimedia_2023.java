5f2df4272e6a7c4ce2f8f68dd80e2732f2c24975
carlangas <carlos.hernandezmar@anahuac.mx>
2023-03-02 20:38:53 -0600

    Programa lee 5 numeros y los suma

--- src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
+++ src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
@@ -13,26 +13,24 @@ public class Prog_multimedia_2023 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); //inicializo el lector de teclado con la clase Scanner (solo copialo)
-        int numLeido = 3;
-        while (numLeido > 0) { /*esto es un bucle y el programa temrinara cuando el numero leido sea = 0*/
+        int numLeido = 1;
+        int i=0;
+        int suma = 0;
+        int suma2 = 0;
+        do { /*esto es un bucle y el programa temrinara cuando el numero leido sea = 0*/
             System.out.print("Ingrese un número entero: ");
-            numLeido = sc.nextInt();  //Leo del teclado lo que escriba y lo guardo como un entero en la variable
-            System.out.print("el numero leido es:" + numLeido + " el mes elegido es: ");
-            /*Apartir de aca creo condiciones para cada numero del 1 al 12*/
-            if (numLeido==1) { System.out.println("Enero"); }
-            else if (numLeido==2) { System.out.println("Febrero"); }
-            else if (numLeido==3) { System.out.println("Marzo"); }
-            else if (numLeido==4) { System.out.println("Abril"); }
-            else if (numLeido==5) { System.out.println("Mayo"); }
-            else if (numLeido==6) { System.out.println("Junio"); }
-            else if (numLeido==7) { System.out.println("Julio"); }
-            else if (numLeido==8) { System.out.println("Agosto"); }
-            else if (numLeido==9) { System.out.println("Septiembre"); }
-            else if (numLeido==10) { System.out.println("Octubre"); }
-            else if (numLeido==11) { System.out.println("Noviembre"); }
-            else if (numLeido==12) { System.out.println("Diciembre"); }
-            else if (numLeido==0) { System.out.println("Salir"); } //Si presiono 0 se sale del bucle y termina el programa
-            else { System.out.println("Error solo acepto del 1 al 12"); }
-        }
+            numLeido = sc.nextInt(); //Leo del teclado lo que escriba y lo guardo como un entero en la variable
+            suma += numLeido;
+            //esta linea es el mismo valor que la anterior:
+            //suma = suma + numLeido;
+            suma2=suma2 + numLeido;  
+            //i=i+1;
+            //i+=1;
+            i++;
+        } while (i < 5);
+        //while (numLeido > 0) //esta condicion es para que el programa entre en bucle mientras el usuario teclea numeros distintos de 0
+       
+        System.out.println("suma:" + suma );
+        System.out.println("suma2:" + suma2 );
     }
 }
