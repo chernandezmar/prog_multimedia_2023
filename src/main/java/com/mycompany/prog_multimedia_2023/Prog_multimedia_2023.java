498fdbdd184673dd13eed9676a0e45fc197a0734
carlangas <carlos.hernandezmar@anahuac.mx>
2023-03-06 20:30:18 -0600

    muestra de como leer arreglos dese un for

--- src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
+++ src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
@@ -11,8 +11,24 @@ package com.mycompany.prog_multimedia_2023;
 public class Prog_multimedia_2023 {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("i vale=" + i);
+        
+        char array[] = {'a','e','i','o','u'};
+        //int array2[] = {1,2,3,5,2,4,1,6};
+        
+        int tamano=array.length;
+        
+        for (int k=0; k<tamano; k++) {
+            System.out.println("indice:" + k + " contenido:" + array[k]);
         }
+        
+        System.out.println(tamano + " contenido" );
+        
+        
+        
+        
+        
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("i vale=" + i);
+//        }
     }
 }
