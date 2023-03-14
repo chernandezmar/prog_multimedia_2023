49c28380de5711afe76e487ea9c11c3dae7809a8
carlangas <carlos.hernandezmar@anahuac.mx>
2023-03-13 20:45:09 -0600

    Se realizo el programa de ejercicio

--- src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
+++ src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
@@ -3,32 +3,58 @@
  */
 
 package com.mycompany.prog_multimedia_2023;
+import java.util.Scanner;
 
-/**
- *
- * @author khemtz
- */
+
 public class Prog_multimedia_2023 {
 
     public static void main(String[] args) {
+ /*       
+- Pedira 10 numeros
+- Solo pueden ser 1 al 20
+- numMayor=0
+- numMenor=21
+- Suma=0
+- numLeido
+- contNumero=1
         
-        char array[] = {'a','e','i','o','u'};
-        //int array2[] = {1,2,3,5,2,4,1,6};
+1.- Mostrar leyenda "Dame un numero:", numero que lea lo  guardare en una variable llamada numLeido
+1a.- Validamos que numLeido < 21, si: continua,no: regresar al paso 1
+2.- contNumero se incremente
+3.- numLeido > numMayor? si: numMayor=numLeido, no: continua
+4.- numLeido < numMenor? si: numMenor=numLeido, no: continua
+5.- suma=suma+numLeido
+6.- conNumero < 11? si: Imprime las variables, regresar al paso 1.*/
+
+
+        int contNumero = 1;
+        int numMayor = 0;
+        int numMenor = 21;
+        int suma = 0;
+        int numLeido;
+        Scanner sc = new Scanner(System.in);
+
         
-        int tamano=array.length;
-        
-        for (int k=0; k<tamano; k++) {
-            System.out.println("indice:" + k + " contenido:" + array[k]);
+        while (contNumero < 11) {
+            System.out.print("Dame un número: ");
+            numLeido = sc.nextInt();  
+            if ((numLeido < 20) && (numLeido > 0)){
+                contNumero++;
+                if (numLeido > numMayor) {
+                    numMayor = numLeido;
+                }
+                if (numLeido < numMenor) {
+                    numMenor = numLeido;
+                }
+                suma = suma + numLeido;                        
+            }  
+            else {
+                System.out.println("Error solo acepto numeros del 1 al 20");
+            }
         }
-        
-        System.out.println(tamano + " contenido" );
-        
+        System.out.println("mayor=" + numMayor +" menor="+ numMenor +" suma="+ suma);
         
         
-        
-        
-//        for (int i = 0; i < 5; i++) {
-//            System.out.println("i vale=" + i);
-//        }
     }
+
 }
