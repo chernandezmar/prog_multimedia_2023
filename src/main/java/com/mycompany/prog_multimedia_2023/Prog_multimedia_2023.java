f25ca03e2aa030a0319e3e2d1c717511b58e3d97
carlangas <carlos.hernandezmar@anahuac.mx>
2023-03-27 20:22:52 -0600

    Se utilizo un ArrayList para hacerlo variable

--- src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
+++ src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
@@ -3,14 +3,33 @@
  */
 
 package com.mycompany.prog_multimedia_2023;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Prog_multimedia_2023 {
     public static void main(String[] args) {
-        System.out.println("hola chicos");
+        int maxFibo;
+        int i1=0, i2=0, sum=0;
+        //int fibo[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //el problema aqui es que no es posible asignar un tamaño variable en este tipo de declaracion
+        ArrayList<Integer> fibo = new ArrayList<Integer>();
         
+        Scanner sc = new Scanner(System.in);
+        System.out.println("dame el tamaño de fibinacci que deseas:");
+        maxFibo=sc.nextInt();
+        
+        for (int i=0; i < maxFibo; i++) {
+            if (i<2) {
+                fibo.add(1);
+            }
+            else {
+                i1= fibo.get(i-1);
+                i2=fibo.get(i-2);
+                sum=i1+i2;
+                fibo.add(sum);
+            }
+        }
+        System.out.println(fibo.toString());
     }
 }
-|
