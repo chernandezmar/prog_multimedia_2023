7c926656c7d0dadb5eb113ee4ddce81564fe48ff
carlangas <carlos.hernandezmar@anahuac.mx>
2023-04-24 20:31:21 -0600

    Ejemplo de como leer un archivo

--- src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
+++ src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
@@ -1,91 +1,37 @@
 package com.mycompany.prog_multimedia_2023;
-import java.util.ArrayList;
-import java.util.Scanner;
+//import java.util.ArrayList;
+//import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Prog_multimedia_2023 {
-
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int numero = 10;
-       int resultado = 0;
-       do {
-            imprime_menu();
-            System.out.print("Ingrese la opcion deseada: ");
-            numero = sc.nextInt();  
-            if (numero == 1 ) {
-                resultado=suma_numeros();
-                System.out.println("el resultado de la suma es:"+ resultado);
-            } 
-            if (numero == 2 ) {
-                fibonacci();
-            } 
-            if (numero == 4) {
-                int a = 20;
-                int b = 57;
-                int res = multiplica_numeros(a, b);
-                System.out.println("el resultado de la operacion es:"+ res);
+      String archivo = "C:\\prog_multimedia_2023\\prog_multimedia_2023\\calificaciones.txt";
+      double suma=0;
+      double promedio=0;
+      try {
+        BufferedReader lector = new BufferedReader(new FileReader(archivo));
+        for (int i=0; i<5; i++){
+            suma=0;
+            String linea[] = lector.readLine().split(",");
+            System.out.println(linea[0] + "***********************");
+            for (int x=1; x<=3; x++) {
+                String valor = linea[x];
+                suma += Double.parseDouble(valor);
+                System.out.print(valor + " " );
             }
-       } while (numero > 0);
-       
-       
-       
-      
-    }
-
-    private static void imprime_menu() {
-        System.out.println( "*******************************************");
-        System.out.println( "****************** MENU ******************");
-        System.out.println( "1.- Suma de de numeros");
-        System.out.println( "2.- Obtener Serie Fibonacci");
-        System.out.println( "3.- Calcular Mayor, Menor, Avg de numeros");
-        System.out.println( "4.- multiplica 2 numeros");
-        System.out.println( "*******************************************");
-    }
-    
-    private static int suma_numeros() {
-        Scanner sc = new Scanner(System.in); //inicializo el lector de teclado con la clase Scanner (solo copialo)
-        int numLeido = 1;
-        int i=0;
-        //int suma = 0;
-        int suma2 = 0;
-        do { 
-            System.out.print("Ingrese un número entero: ");
-            numLeido = sc.nextInt(); //Leo del teclado lo que escriba y lo guardo como un entero en la variable
-            suma2=suma2 + numLeido;  
-            i++;
-        } while (i < 5);
-        return suma2;
-    }
-    
-    private static void fibonacci() {
-        int maxFibo;
-        int i1=0, i2=0, sum=0;
-        //int fibo[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //el problema aqui es que no es posible asignar un tamaño variable en este tipo de declaracion
-        ArrayList<Integer> fibo = new ArrayList<Integer>();
-        
-        Scanner sc = new Scanner(System.in);
-        System.out.println("dame el tamaño de fibinacci que deseas:");
-        maxFibo=sc.nextInt();
-        
-        for (int i=0; i < maxFibo; i++) {
-            if (i<2) {
-                fibo.add(1);
-            }
-            else {
-                i1= fibo.get(i-1);
-                i2=fibo.get(i-2);
-                sum=i1+i2;
-                fibo.add(sum);
-            }
+            promedio = suma/3;
+            System.out.println(","+ String.format("%.1f", promedio));
         }
-        System.out.println(fibo.toString());
+        lector.close();
+      } catch (IOException e) {
+          System.out.println("Error al leer el archivo" + archivo );
+      }
+      
+   
     }
-    
-    public static int multiplica_numeros(int n1, int n2) {
-        int resultado=0;
-        resultado=n1*n2;
-        return resultado;
-    }
-    
-}
 
+}
