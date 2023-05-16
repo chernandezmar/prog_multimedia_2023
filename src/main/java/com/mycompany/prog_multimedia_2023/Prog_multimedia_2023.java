f35dee58fb1d6269dfa429ff357c79ea87d0e582
carlangas <carlos.hernandezmar@anahuac.mx>
2023-05-15 19:58:19 -0600

    Se agrego el codigo para escribir informaicon en un archivo

--- src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
+++ src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
@@ -2,40 +2,30 @@ package com.mycompany.prog_multimedia_2023;
 //import java.util.ArrayList;
 //import java.util.Scanner;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+//import java.io.BufferedReader; 
+import java.io.FileWriter;//libreria para escribir en archivos
+import java.io.FileReader;//Libreria para leer archivos
 import java.io.IOException;
-import java.util.Arrays;
+//import java.util.Arrays;
 
 
 public class Prog_multimedia_2023 {
     public static void main(String[] args) {
-      String archivo = "C:\\prog_multimedia_2023\\prog_multimedia_2023\\sismos2.csv";
-      Double SismoMayor=0.0;
-      String Datos = "";
+      String archivo = "C:\\prog_multimedia_2023\\prog_multimedia_2023\\prueba_escritura.txt";
+      String texto="Esta linea fue escrita en el programa de java";
       
       try {
-        BufferedReader lector = new BufferedReader(new FileReader(archivo));
-        for (int i=0; i<274643; i++) {
-            String linea = lector.readLine();
-            if (linea.contains("no calculable")) {
-                //System.out.println("nop");
-            } else {
-                String ArrayLinea[] = linea.split(",");
-                String valor = ArrayLinea[2];
-                //System.out.println(i + " " + ArrayLinea[2]);
-                Double magnitud =  Double.parseDouble(valor);
-                if (SismoMayor < magnitud) {
-                    SismoMayor=magnitud;
-                    Datos = linea;
-                }
-            }
-            
-        }
+         FileWriter escribeArchivo = new FileWriter(archivo,true);
+         escribeArchivo.write(texto+"\n");
+         escribeArchivo.write("Esta es otra linea\n");
+         escribeArchivo.write(texto+"\n");
+         escribeArchivo.write("*****************************************\n\n");
+         escribeArchivo.close(); 
+         System.out.println("Se escribio correctamente en el archivo");
+         
       } catch (IOException e) {
-        System.out.println("Error al leer el archivo" + archivo );
-        }
-      
-      System.out.println("El Mayor Sismo fue:" + Datos );
+          System.out.println("Error al escribir en el archivo: " + e.getMessage());
+      }
+
     }
 }
