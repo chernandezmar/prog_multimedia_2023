a2667c1b6123915a578f6772a88451255300c295
carlangas <carlos.hernandezmar@anahuac.mx>
2023-05-15 20:12:44 -0600

    Obtiene reporte de sismos mayores a 6.5

--- src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
+++ src/main/java/com/mycompany/prog_multimedia_2023/Prog_multimedia_2023.java
@@ -4,6 +4,7 @@ package com.mycompany.prog_multimedia_2023;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -11,11 +12,16 @@ import java.util.Arrays;
 public class Prog_multimedia_2023 {
     public static void main(String[] args) {
       String archivo = "C:\\prog_multimedia_2023\\prog_multimedia_2023\\sismos2.csv";
+      String archivoSalida = "C:\\prog_multimedia_2023\\prog_multimedia_2023\\sismos_mayores_5.txt";
+      
+      
       Double SismoMayor=0.0;
       String Datos = "";
       
       try {
         BufferedReader lector = new BufferedReader(new FileReader(archivo));
+        FileWriter escribeArchivo = new FileWriter(archivoSalida,false);
+        
         for (int i=0; i<274643; i++) {
             String linea = lector.readLine();
             if (linea.contains("no calculable")) {
@@ -25,6 +31,9 @@ public class Prog_multimedia_2023 {
                 String valor = ArrayLinea[2];
                 //System.out.println(i + " " + ArrayLinea[2]);
                 Double magnitud =  Double.parseDouble(valor);
+                if (magnitud > 6.5){
+                    escribeArchivo.write(linea+"\n");
+                }
                 if (SismoMayor < magnitud) {
                     SismoMayor=magnitud;
                     Datos = linea;
@@ -32,6 +41,7 @@ public class Prog_multimedia_2023 {
             }
             
         }
+        escribeArchivo.close();
       } catch (IOException e) {
         System.out.println("Error al leer el archivo" + archivo );
         }
